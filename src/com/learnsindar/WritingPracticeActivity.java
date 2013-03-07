4c8acd6b899bbc64d60cf500cac838d02586823e
dgs3 <sayles.dave@gmail.com>
2013-03-07 00:11:19 -0500

    Updated layout for WritingPractice

--- src/com/learnsindar/WritingPracticeActivity.java
+++ src/com/learnsindar/WritingPracticeActivity.java
@@ -4,53 +4,53 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MenuItem;
 import android.view.MotionEvent;
-import android.view.SurfaceView;
 import android.view.View.OnTouchListener;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Vector;
 import java.util.Iterator;
 
 class DrawView extends View implements OnTouchListener {
     
-    List<Point> points  = new ArrayList<Point>();
+    Vector<Path> paths = new Vector<Path>();
+    Path path;
     Paint paint = new Paint();
 
     public DrawView(Context context){
         super(context);
-        Log.v(MainActivity.logtag, "creating draw view");
-        setFocusable(true);
         setFocusableInTouchMode(true);
+        path = new Path();
         this.setOnTouchListener(this);
         paint.setColor(Color.WHITE);
+        paint.setStyle(Style.STROKE);
+        paint.setStrokeWidth(3);
         paint.setAntiAlias(true);
-        Log.v(MainActivity.logtag, "created draw view");
     }
 
     @Override
     protected void onDraw(Canvas canvas){
-        for(Point point : points){
-            canvas.drawCircle(point.x, point.y, 5, paint);
+        for(Path path : paths){
+            canvas.drawPath(path, this.paint);
         }
-        //canvas.drawPath(this.path, this.paint);
+        canvas.drawPath(this.path, this.paint);
     }
 
     public boolean onTouch(View view, MotionEvent event){
-        /*if(event.getAction() == MotionEvent.ACTION_DOWN){
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
             this.path = new Path();
             this.path.moveTo(event.getX(), event.getY());
             this.path.lineTo(event.getX(), event.getY());
@@ -58,37 +58,46 @@ class DrawView extends View implements OnTouchListener {
             this.path.lineTo(event.getX(), event.getY());
         }else if(event.getAction() == MotionEvent.ACTION_UP){
             this.path.lineTo(event.getX(), event.getY());
+            this.paths.add(this.path);
         }
-        return true;*/
-        Point point = new Point();
-        point.x = event.getX();
-        point.y = event.getY();
-        points.add(point);
         invalidate();
-        return true;       
+        return true;
     }
 
 }
 
-class Point{
-    float x, y;
-    } 
-
 public class WritingPracticeActivity extends Activity {
 
     DrawView drawView;
+    Button nextCharButton;
+    RelativeLayout writingLayout;
+    TextView charDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        Log.v(MainActivity.logtag, "creating writing practice activity");
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        writingLayout = new RelativeLayout(this);
+        charDisplay = new TextView(this);
+        charDisplay.setText("asdfasdf");
+        nextCharButton = new Button(this);
+        nextCharButton.setText("FUCK");
+        nextCharButton.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v){
+            nextCharButton.setText("OMG");
+            }
+        });
+        nextCharButton.setVisibility(View.VISIBLE);
         drawView = new DrawView(this);
-        setContentView(drawView);
-        drawView.requestFocus();
-        Log.v(MainActivity.logtag, "Created writing practice activity");
-        //Intent intent = getIntent();
-        //Resources res = getResources();
+        writingLayout.setGravity(1);
+        writingLayout.addView(drawView);
+        writingLayout.addView(nextCharButton);
+        writingLayout.addView(charDisplay);
+        setContentView(writingLayout);
+    }
+
+    void
+    nextWritingChar(View view){
+        int x= 0;
     }
 }
